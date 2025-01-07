8eb3325591391cc197c6e6c8019a9178b64e214f
yerger1520 <88238730+eren1520@users.noreply.github.com>
2025-01-07 15:40:17 +0700

    update-Folder

---
+++ shoppe-api/src/main/java/com/example/shoppe_api/ShoppeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeApiApplication.class, args);
+	}
+
+}
---
+++ shoppe-api/src/test/java/com/example/shoppe_api/ShoppeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
